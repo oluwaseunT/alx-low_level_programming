aafcb53d2e605fc192017c0f10cf3c8b1a0b0a4f
oluwaseunT <taiwooluwaseun658@gmail.com>
2022-10-13 21:46:53 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-  * main -prints Programming is like building a multilingual puzzle
-  * Return: (0);
+ * main - print Programming is like buiding a multilingual puzzle
+ * Return: 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
      
