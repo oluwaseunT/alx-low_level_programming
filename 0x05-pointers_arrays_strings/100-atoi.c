8e4dfa555d9d5fefdb9c180f60bfcae76e248b10
oluwaseunT <taiwooluwaseun658@gmail.com>
2022-12-24 00:43:29 -0800

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,37 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer.
- * @s: input string.
- * Return: integer.
-*/
+ * _atoi - Convert a string to an integer.
+ * @s: the number to convert
+ * Return: A integer
+ */
 int _atoi(char *s)
 {
-unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-while (*(s + count) != '\0')
-{
-if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-break;
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
 
-if (*(s + count) == '-')
-pn *= -1;
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
+	}
 
-if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-{
-if (size > 0)
-m *= 10;
-size++;
-}
-count++;
-}
-
-for (i = count - size; i < count; i++)
-{
-oi = oi + ((*(s + i) - 48) * m);
-m /= 10;
-}
-return (oi * pn);
+	ni *= min;
+	return (ni);
 }
